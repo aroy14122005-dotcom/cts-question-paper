f887d4766c5f1ef146d61cb548d52cee75e6f118
Ankit <soumajitx@gmail.com>
2026-06-28 21:55:14 +0530

    Fix static files

--- config/settings.py
+++ config/settings.py
@@ -146,6 +146,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
